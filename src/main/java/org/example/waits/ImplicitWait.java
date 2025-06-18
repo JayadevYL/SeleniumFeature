8e11b02e0a42347f1e3c85b892245fed55de0fac
Jayadev YL <jayadevylgowda10@gmail.com>
2025-06-18 08:02:32 +0530

    implicit wait 1st push

--- src/main/java/org/example/waits/ImplicitWait.java
+++ src/main/java/org/example/waits/ImplicitWait.java
@@ -1,5 +1,15 @@
 package org.example.waits;
 
-public class ImplicitWait {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+public class ImplicitWait {
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://selectorshub.com/xpath-practice-page");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+    }
 }
